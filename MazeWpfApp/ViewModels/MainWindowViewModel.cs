c94e5aa1d3e225b9d04a08d2c511818e05974254
Katarzyna Czerwinska <s17098@pjwstk.edu.pl>
2021-02-02 21:34:58 +0100

    #12 Visualization of GameBoardView in MainWindowView

--- MazeWpfApp/ViewModels/MainWindowViewModel.cs
+++ MazeWpfApp/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using MazeWpfApp.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace MazeWpfApp.ViewModels
 {
-    class MainWindowViewModel
+    public class MainWindowViewModel
     {
+        public MainWindowViewModel()
+        {
+            View = new GameBoardView();
+        }
+
+        public UserControl View { get; set; }
     }
 }
